0bbd0f48c259b61801afb125301c294538e32759
Daniel Pittman <dpdanpittman@gmail.com>
2023-04-06 18:52:36 -0600

    update locales

--- webapp/src/plugins/i18n.ts
+++ webapp/src/plugins/i18n.ts
@@ -1,13 +1,11 @@
 import { createI18n } from 'vue-i18n';
 import en from '../locales/en.json';
-import pl from '../locales/pl.json';
 
 export default createI18n({
   legacy: false,
   locale: 'en',
   fallbackLocale: 'en',
   messages: {
-    en,
-    pl,
+    en
   }
 });
